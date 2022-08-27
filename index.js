3752fec164341462ac47d82cb9bc26c27093d2ac
Dan Muendo <danwalker254254@gmail.com>
2022-08-27 06:39:32 -0400

    Assignment complete

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  var l = word.length;
+  
+  for (var i = 0; i < l / 2; i++) {
+      if (word.charAt(i) !== word.charAt(l - 1 - i)) {
+          return false;
+      }
+  }
+  return true;
 }
+isPalindrome("racecar");
 
 /* 
   Add your pseudocode here
+  1. get length of word
+  2. iterate over each character in the word
+  3. If the first character is not equal to the last character 
+  return false else true
+  
 */
 
 /*
   Add written explanation of your solution here
+  Check number of characters the passed in argument(word)
+  Go through the characters checking if they match the character at their co-responding index
 */
 
 // You can run `node index.js` to view these console logs
